7046fbdd79b16c8d1b6b006965dcdf6eb44118e9
rninne <rninne@gmail.com>
2012-08-04 14:18:08 +1000

    :s

--- credentials.js
+++ credentials.js
@@ -7,10 +7,6 @@ function generate () {
         "demo11.service-now.com": {
         "user_name": "admin",
         "password": "admin"
-        },
-        "citecsandpit.service-now.com": {
-        "user_name": "ninnesr_admin",
-        "password": "c!W28q7e69s5c"
         }
     }
 }
